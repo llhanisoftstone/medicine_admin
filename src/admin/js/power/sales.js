ea482ebd1d5053462060f6dad294f0d5d24730d0
陈文越 <1104499817@qq.com>
2018-09-03 21:01:38 +0800

    统计管理模块所有页面

---
+++ src/admin/js/power/sales.js
@@ -0,0 +1,31 @@
+/**
+ * Created by pc on 2018/9/3.
+ */
+//var base_url='/rs/v_member';
+var currentPageNo = 1;
+var pageRows = 10;
+$(function() {
+    var page = getUrlParamsValue("page");
+    if(page&&page != "undefined"){
+        currentPageNo = page;
+    }
+    $("#resetSearchBtn", $(".report")).bind("click", function(){
+        $("#reasonSearchForm", $(".report"))[0].reset();
+        currentPageNo = 1;
+        pageRows = 10;
+        //queryList();
+    });
+    $("#searchBtn", $(".report")).unbind("click");
+    $("#searchBtn", $(".report")).bind("click", showSearchPage);
+    //queryList();
+});
+function showSearchPage(){
+    $(".reasonSearch", $(".report")).animate({
+        height : 'toggle',
+        opacity : 'toggle'
+    }, "slow");
+    if($(".reasonSearch").is(":visible")){
+        isSearch = false;
+    }
+}
+
