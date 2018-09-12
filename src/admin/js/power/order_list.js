0fa5c522552a3de97be2e9200a29534e5d4c44bf
陈文越 <1104499817@qq.com>
2018-09-12 21:46:53 +0800

    bug修改

--- src/admin/js/power/order_list.js
+++ src/admin/js/power/order_list.js
@@ -53,60 +53,10 @@ function bindDataToggleModal(){
         $("#progressDiv").css("top", top-100);
     });
 }
-// function addRemarks(obj,id,remark){
-//     // saveFormData("buildProjectSearchForm");
-//     if(remark){
-//         $("#remark_admin").val(remark);
-//     }else{
-//         $("#remark_admin").val("");
-//     }
-//     $("#sureSaveaddremarkModal").unbind().bind("click",function(){
-//         var remark_admin = $("#remark_admin").val();
-//         if(remark_admin == ""){
-//             $("#remark_admin").focus();
-//             showError("请输入备注内容");
-//             return;
-//         }
-//         var data = {
-//             remark_admin:remark_admin
-//         }
-//         zhput(base_url+"/"+ id,data).then(function(result){
-//             if(result.code == 200){
-//                 $(obj).parent().parent().find(".newremark").html("备注信息:"+remark_admin);
-//                 $("#closeaddremarkModal").trigger("click");
-//                 getpageRecord();
-//                 queryList();
-//             }
-//         })
-//     })
-// }
 
-
-
-// 弹出发货框按钮事件----单产品发货
-// function showOrderLogiModal(subOrderNumber, orderNumber, logisCode, logisNumber) {
-//     // saveFormData("buildProjectSearchForm");
-//     if (logisCode && logisNumber && logisCode != "null"&& logisNumber != "null") {
-//         $("#iOldLogiId").val(logisCode);
-//         $("#iLogiNum").val(logisNumber);
-//     } else {
-//         $("#iOldLogiId").val('0');
-//         $("#iLogiNum").val('');
-//     }
-//     loadOrderLogi(orderNumber,subOrderNumber);
-// }
-// function refund(element, orderCode, detailId,iPrice,mainId,mainnum,activity_category){
-//     if($(element).attr("data-type")==4){
-//         $(".refund_price_input").hide();
-//     }else{
-//         $(".refund_price_input").show();
-//     }
-//     loadOrderReturnPriceModel(orderCode, detailId, iPrice,mainId,mainnum);
-// }
 function chargeoff(id,name,o_id){
     var data = {
         status:1,
-        //order_id:o_id
     }
     if(confirm("您确定要核销"+name+"吗？")){
         zhput("/rs/member_ticket/"+ id,data).then(function(result){
@@ -120,39 +70,14 @@ function chargeoff(id,name,o_id){
     }
 
 }
-// function getCompany(comp) {
-//
-//     var data={
-//         store_id:store_id
-//     }
-//     zhget(base_url,data).then(function (result) {
-//         console.log(result)
-//         if(result.code==200) {
-//             buildTableNoPage(result, 'brand-template', 'comp');
-//             initselect('comp');
-//             $.hideActionLoading();
-//         }else{
-//             buildTableNoPage(result, 'brand-template', 'comp');
-//             initselect('comp');
-//             $.hideActionLoading();
-//         }
-//         buildTableByke(result, 'event-template', 'event-placeholder','paginator',queryList,pageRows);
-//     })
-// }
-// function initselect(id){
-//     $('#'+id).selectpicker({
-//         size: 10,
-//         width:'100%'
-//     });
-// }
 function timestampToTime(timestamp) {
     var date = new Date(timestamp);//时间戳为10位需*1000，时间戳为13位的话不需乘1000
     var Y = date.getFullYear() + '-';
     var M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-';
     var D = date.getDate() + ' ';
-    var h = date.getHours() + ':';
-    var m = date.getMinutes() + ':';
-    var s = date.getSeconds();
+    var h = (date.getHours()< 10 ? '0'+(date.getHours()) : date.getHours())+ ':';
+    var m = (date.getMinutes()< 10 ? '0'+(date.getMinutes()) : date.getMinutes())+ ':';
+    var s = (date.getSeconds()< 10 ? '0'+(date.getSeconds()) : date.getSeconds());
     return Y+M+D+h+m+s;
 }
 function createInfoList(result){
@@ -166,47 +91,17 @@ function createInfoList(result){
         var end=Number(date)+Number(toEnd);
         timestampToTime(end);
         mainOrder.end_time=timestampToTime(end);
-        //result.rows.end_time=result.rows.get_time+
-        // if(mainOrder.old_address){
-        //     mainOrder.oldaddress = 1;
-        // }
-        // if(mainOrder.rq_list && (mainOrder.status == 2) ){
-        //     result.rows[i].trcolpan=2+mainOrder.rq_list.length;
-        // }else{
-        //     result.rows[i].trcolpan=2;
-        // }
-        // if(mainOrder.rq_list){
-        //     for(var k=0,len=mainOrder.rq_list.length;k<len;k++){
-        //         mainOrder.rq_list[k].main_order_id=mainOrder.id;
-        //     }
-        // }
         mainOrder.rowNum = (currentPageNo - 1) * pageRows + i + 1;
     }
     buildTableByke(result, 'event-template', 'event-placeholder','paginator',queryList,pageRows);
-    // $(".refundBtn").each(function () {
-    //     var status = $(this).attr("status");
-    //     var amount = $(this).attr("amount");
-    //     if (amount == 0 || status == 0 || status == 6 || status == 8 || status == 9) {
-    //         $(this).hide();
-    //     }
-    // });
 }
 function queryList() {
-    //  download=="download"  时，为导出数据
     var data={
         page:currentPageNo,
         size:pageRows,
         store_id:store_id,
-        //order:'create_time desc'
+        order:"status asc",
     };
-    // if(download=="download"){
-    //     data={
-    //         //order:'create_time desc',
-    //         //download:1,
-    //         store_id:store_id
-    //     }
-    //
-    // }
     if(isSearch){
         var count=$("#order_count").val();
         var status=$("#status").val();
@@ -235,32 +130,10 @@ function queryList() {
         }else {
             data.get_time = undefined;
         }
-        // var zhiTimeEnd1 = zhiTimeEnd +" 23-59-59";
-        // if(zhiTimeStart!=""&&zhiTimeEnd!=""){
-        //     data.end_time='>=,'+zhiTimeStart+',<=,'+zhiTimeEnd1;
-        // }else if(zhiTimeStart!=""&&zhiTimeEnd==""){
-        //     data.end_time='>=,'+zhiTimeStart;
-        // }else if(zhiTimeStart==""&&zhiTimeEnd!=""){
-        //     data.end_time='<=,'+zhiTimeEnd1;
-        // }else {
-        //     data.end_time = undefined;
-        // }
+
         data.search=1;
     }
 
-    // if(download=="download"){
-    //     zhpost(base_url, data).then(function (result) {
-    //         debugger
-    //         $.hideActionLoading();
-    //         if(checkData(result,'get','queryList','table-orderlist')) {
-    //
-    //             if(download=="download"){
-    //                 window.location.href = targetUrl + result.path;
-    //             }
-    //         }
-    //     });
-    //     return;
-    // }
     zhget(base_url, data).then(function (result) {
         $.hideActionLoading();
         if(checkData(result,'get','queryList','table-orderlist')) {
@@ -273,12 +146,7 @@ function tabcomp(){
     $(".selectCategoryId").val("");
 }
 Handlebars.registerHelper('ticketsetstatus', function(v1) {
-    // 状态：0-待付款；1-已付款/待发货；2-已发货/待收货;
-    // 4-交易成功；5-交易取消；6-已消费(卡券已核销)；8-交易失效；
-    // 9-退款中；10-退款成功；11-退款失败,12，退款拒绝，票务系统使用
-//     代金券购买成功后就是待使用状态，
-// 待付款status=0  待使用status=4  已使用status=6    失效status=8
-   if(v1==1){
+    if(v1==1){
         return "已核销";
     }else if(v1==2){
         return "已过期";
@@ -286,3 +154,4 @@ Handlebars.registerHelper('ticketsetstatus', function(v1) {
         return "未核销";
     }
 });
+
