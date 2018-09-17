86adf3671f4cdd409b73f9c9efa34b87a71cdb6a
GaoXinwu <gaoxinwucn@foxmail.com>
2018-09-17 12:53:32 +0800

    22235 （后台）关卡设置排序：1.顺序（从大到小）2.状态（启用、禁用）3.创建时间倒叙

--- src/admin/js/power/gamelist.js
+++ src/admin/js/power/gamelist.js
@@ -35,7 +35,7 @@ function queryList(){
     var data={
         page: currentPageNo,
         size: pageRows,
-        order:'status asc, create_time desc',
+        order:'order_code desc, status desc, create_time desc',
         id:'>,10',
         status:'<>,99'
     }
